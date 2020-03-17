9e6e326e84dcfa5f223e79561141b650a69792d4
taoqf <tao_qf@163.com>
2020-03-17 10:16:42 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,34 @@
+import { getExtension } from 'mime';
+import fetch from 'node-fetch';
+import { v4 } from 'uuid';
 
-export default function an23(param1: string) {
+export default async function download(url: string) {
+	const res = await fetch(url, {
+		timeout: 60000
+	});
+	if (res.status > 299) {
+		throw new Error(await res.text());
+	}
+	const headers = res.headers;
+	const data = await res.buffer();
+	const name = (() => {
+		const cd = headers.get('content-disposition');
+		if (cd) {
+			return decodeURIComponent(cd.replace(/.*filename=/, ''));
+		}
+		const type = headers.get('Content-Type');
+		const id = v4();
+		if (type) {
+			const ext = getExtension(type);
+			if (ext) {
+				return `${id}.${ext}`;
+			}
+			return id;
+		}
+		return id;
+	})();
+	return {
+		data,
+		name
+	};
 }
--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,23 @@ const test = require('ava');
 
 const { default: a } = require('../dist/index');
 
-test('xxx', (t) => {
-	t.pass();
+
+test('下载成功', async (t) => {
+	const r = await a('http://127.0.0.1:8889/fsweb/getfile?id=06e75b24-0152-4fdb-8572-9c23a97396b2&download');
+	t.is(r.name, 'Screenshot from 2020-02-07 21-19-26.png');
+	t.not(r.data, null);
+});
+
+test('下载成功,不加download参数', async (t) => {
+	const r = await a('http://127.0.0.1:8889/fsweb/getfile?id=06e75b24-0152-4fdb-8572-9c23a97396b2');
+	t.is(r.name, 'Screenshot from 2020-02-07 21-19-26.png');
+	t.not(r.data, null);
+});
+
+test('下载失败', async (t) => {
+	const err = await t.throwsAsync(async () => {
+		const r = await a('http://127.0.0.1:8889/fsweb/getfile?id=xxx.mp4&download');
+		console.log(r);
+	});
+	t.is(err.message, 'Not Found');
 });
